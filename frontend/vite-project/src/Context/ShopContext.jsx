802b6c7c8c2ea0ae828dfae1145b1c620629189a
Reinaldo Romero <reinaldoromero696@gmail.com>
2023-11-25 22:34:44 -0400

    commit 14

--- frontend/vite-project/src/Context/ShopContext.jsx
+++ frontend/vite-project/src/Context/ShopContext.jsx
@@ -1,12 +1,21 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import all_product from '../Components/Assets/all_product';
 
 export const ShopContext = createContext(null);
 
+const getDeafultcart = () => {
+    let cart = {};
+    for (let index = 0; index < all_product.length; index++) {
+        cart[index] = 0;
+    }
+    return cart;
+}
+
 const ShopContextProvider = (props) => {
 
-    const contextValue = {all_product};
-
+    const [cartItems,setCartItems] = useState(getDeafultcart());
+    const contextValue = {all_product,cartItems};
+    
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
